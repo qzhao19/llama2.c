9e2e063b41752369f909a6a543810918715f2b0b
qzhao19 <qi.zhao@outlook.fr>
2025-05-19 22:13:33 +0800

    create run.hpp

---
+++ run.hpp
@@ -0,0 +1,21 @@
+#ifndef RUN_HPP_
+#define RUN_HPP_
+
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cstdint>
+#include <ctime>
+#include <cmath>
+#include <cstring>
+#include <fcntl.h>
+#if defined _WIN32
+    #include "win.h"
+#else
+    #include <unistd.h>
+    #include <sys/mman.h>
+#endif
+
+
+
+#endif // RUN_HPP_
